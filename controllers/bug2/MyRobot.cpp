319ce8131e06a8aa366c604376451f293afc472d
Fran271103 <37521955+Fran271103@users.noreply.github.com>
2025-04-28 11:22:25 +0200

    Initial commit

---
+++ controllers/bug2/MyRobot.cpp
@@ -0,0 +1,228 @@
+#include "MyRobot.h"
+#include <cmath>
+#include <webots/Robot.hpp>
+#include <iostream>
+
+MyRobot::MyRobot() {
+    // Initialize motors
+    _left_motor = getMotor("left wheel motor");
+    _right_motor = getMotor("right wheel motor");
+    _left_motor->setPosition(INFINITY);
+    _right_motor->setPosition(INFINITY);
+    _left_motor->setVelocity(0.0);
+    _right_motor->setVelocity(0.0);
+
+    // Initialize position sensors
+    _left_wheel_sensor = getPositionSensor("left wheel sensor");
+    _right_wheel_sensor = getPositionSensor("right wheel sensor");
+    _left_wheel_sensor->enable(TIME_STEP);
+    _right_wheel_sensor->enable(TIME_STEP);
+
+    // Initialize compass
+    _my_compass = getCompass("compass");
+    _my_compass->enable(TIME_STEP);
+
+    // Initialize GPS
+    _my_gps = getGPS("gps");
+    _my_gps->enable(TIME_STEP);
+
+    // Initialize distance sensors
+    for (int i = 0; i < 16; i++) {
+        char buffer[10];
+        sprintf(buffer, "ds%d", i);
+        _distance_sensor[i] = getDistanceSensor(buffer);
+        _distance_sensor[i]->enable(TIME_STEP);
+    }
+
+    // Initialize cameras
+    _front_cam = getCamera("camera_f");
+    _spher_cam = getCamera("camera_s");
+    _front_cam->enable(TIME_STEP);
+    _spher_cam->enable(TIME_STEP);
+
+    // Initialize odometry
+    _sl = encoder_tics_to_meters(_left_wheel_sensor->getValue());
+    _sr = encoder_tics_to_meters(_right_wheel_sensor->getValue());
+
+    // Initialize position and orientation
+    _x = 0.0;
+    _y = 0.0;
+    _theta = 0.0;
+
+    // Set goal
+    _x_goal = _path_goal[_active_point][0];
+    _y_goal = _path_goal[_active_point][1];
+    _theta_goal = atan2(_y_goal - _y, _x_goal - _x);
+
+    // Initialize mode for Bug2
+    _mode = 0; // Start in go-to-goal mode
+}
+
+MyRobot::~MyRobot() {
+    // Cleanup if necessary
+}
+
+void MyRobot::run() {
+    while (step(TIME_STEP) != -1) {
+        compute_odometry(true);
+        print_odometry();
+        if (goal_reached()) {
+            if (_active_point < _total_points - 1) {
+                _active_point++;
+                _x_goal = _path_goal[_active_point][0];
+                _y_goal = _path_goal[_active_point][1];
+                _theta_goal = atan2(_y_goal - _y, _x_goal - _x);
+            } else {
+                stop();
+                break;
+            }
+        }
+        go_route();
+    }
+}
+
+void MyRobot::go_route() {
+    if (goal_reached()) {
+        // Stop the robot
+        _left_motor->setVelocity(0.0);
+        _right_motor->setVelocity(0.0);
+        return;
+    }
+
+    if (_mode == 0) {
+        // Check for obstacles
+        bool obstacle_ahead = false;
+        if (_distance_sensor[14]->getValue() < 0.1 || // Left-front
+            _distance_sensor[15]->getValue() < 0.1 || // Front-left
+            _distance_sensor[0]->getValue() < 0.1 ||  // Front
+            _distance_sensor[1]->getValue() < 0.1 ||  // Front-right
+            _distance_sensor[2]->getValue() < 0.1) {  // Right-front
+            obstacle_ahead = true;
+        }
+
+        if (!obstacle_ahead) {
+            // Normal go-to-goal behavior
+            float angle_diff = compute_angle_goal();
+            if (fabs(angle_diff) > DEGREE_TOLERANCE * M_PI / 180.0) {
+                head_goal();
+            } else {
+                move_forward();
+            }
+        } else {
+            // Switch to wall-following mode
+            _mode = 1;
+        }
+    } else if (_mode == 1) {
+        // Wall-following mode
+        float desired_distance = 0.15; // Desired distance from wall
+        float K = 2.0; // Proportional gain
+        float forward_speed = MEDIUM_SPEED; // Base speed
+        float ds14_value = _distance_sensor[14]->getValue(); // Left-front sensor
+        float error = desired_distance - ds14_value;
+        float differential_speed = K * error;
+        float left_speed = forward_speed + differential_speed;
+        float right_speed = forward_speed - differential_speed;
+
+        // Clamp speeds
+        if (left_speed > MAX_SPEED) left_speed = MAX_SPEED;
+        if (left_speed < -MAX_SPEED) left_speed = -MAX_SPEED;
+        if (right_speed > MAX_SPEED) right_speed = MAX_SPEED;
+        if (right_speed < -MAX_SPEED) right_speed = -MAX_SPEED;
+
+        // Set motor speeds
+        _left_motor->setVelocity(left_speed);
+        _right_motor->setVelocity(right_speed);
+
+        // Check if we can switch back to go-to-goal mode
+        float ds0_value = _distance_sensor[0]->getValue(); // Front sensor
+        float angle_diff = compute_angle_goal();
+        if (ds0_value > 0.2 && fabs(angle_diff) < 45.0 * M_PI / 180.0) {
+            _mode = 0; // Switch back to go-to-goal mode
+        }
+    }
+}
+
+void MyRobot::compute_odometry(bool use_compass) {
+    // Get wheel displacements
+    float new_sl = encoder_tics_to_meters(_left_wheel_sensor->getValue());
+    float new_sr = encoder_tics_to_meters(_right_wheel_sensor->getValue());
+    float diff_sl = new_sl - _sl;
+    float diff_sr = new_sr - _sr;
+    _sl = new_sl;
+    _sr = new_sr;
+
+    // Update position
+    _x = _x + ((diff_sr + diff_sl) / 2 * cos(_theta + (diff_sr - diff_sl)/(2 * WHEELS_DISTANCE)));
+    _y = _y + ((diff_sr + diff_sl) / 2 * sin(_theta + (diff_sr - diff_sl)/(2 * WHEELS_DISTANCE)));
+
+    // Update orientation
+    if (use_compass) {
+        _theta = convert_bearing_to_radians();
+    } else {
+        _theta = _theta + ((diff_sr - diff_sl)/WHEELS_DISTANCE);
+        // Wrap theta to [-pi, pi]
+        while (_theta > M_PI) _theta -= 2*M_PI;
+        while (_theta < -M_PI) _theta += 2*M_PI;
+    }
+}
+
+double MyRobot::convert_bearing_to_radians() {
+    const double *north = _my_compass->getValues();
+    double rad = atan2(north[0], north[2]);
+    return rad;
+}
+
+double MyRobot::convert_bearing_to_degrees() {
+    return convert_rad_to_deg(convert_bearing_to_radians());
+}
+
+double MyRobot::convert_deg_to_rad(double deg) {
+    return deg * M_PI / 180.0;
+}
+
+double MyRobot::convert_rad_to_deg(double rad) {
+    return rad * 180.0 / M_PI;
+}
+
+float MyRobot::encoder_tics_to_meters(float tics) {
+    return tics * WHEEL_RADIUS;
+}
+
+void MyRobot::print_odometry() {
+    std::cout << "Position: (" << _x << ", " << _y << "), Orientation: " << convert_rad_to_deg(_theta) << " degrees" << std::endl;
+}
+
+bool MyRobot::goal_reached() {
+    return compute_distance_goal() < DISTANCE_TOLERANCE;
+}
+
+float MyRobot::compute_distance_goal() {
+    return sqrt(pow(_x_goal - _x, 2) + pow(_y_goal - _y, 2));
+}
+
+float MyRobot::compute_angle_goal() {
+    double desired_theta = atan2(_y_goal - _y, _x_goal - _x);
+    double angle_diff = desired_theta - _theta;
+    while (angle_diff > M_PI) angle_diff -= 2*M_PI;
+    while (angle_diff < -M_PI) angle_diff += 2*M_PI;
+    return angle_diff;
+}
+
+void MyRobot::head_goal() {
+    float angle_diff = compute_angle_goal();
+    float speed = angle_diff * 10.0; // Proportional control
+    _left_motor->setVelocity(-speed);
+    _right_motor->setVelocity(speed);
+}
+
+void MyRobot::move_forward() {
+    float distance = compute_distance_goal();
+    float speed = std::min(MEDIUM_SPEED, distance * 5.0); // Proportional speed
+    _left_motor->setVelocity(speed);
+    _right_motor->setVelocity(speed);
+}
+
+void MyRobot::stop() {
+    _left_motor->setVelocity(0.0);
+    _right_motor->setVelocity(0.0);
+}
---
+++ controllers/bug2/MyRobot.h
@@ -0,0 +1,84 @@
+#ifndef MY_ROBOT_H
+#define MY_ROBOT_H
+
+#include <webots/Robot.hpp>
+#include <webots/Motor.hpp>
+#include <webots/PositionSensor.hpp>
+#include <webots/Compass.hpp>
+#include <webots/GPS.hpp>
+#include <webots/DistanceSensor.hpp>
+#include <webots/Camera.hpp>
+
+#define TIME_STEP 64
+#define MAX_SPEED 10.0
+#define MEDIUM_SPEED 5.0
+#define DEGREE_TOLERANCE 5
+#define DISTANCE_TOLERANCE 0.1
+#define WHEELS_DISTANCE 0.3606
+#define WHEEL_RADIUS 0.0825
+#define ENCODER_TICS_PER_RADIAN 1
+
+class MyRobot : public webots::Robot {
+public:
+    MyRobot();
+    ~MyRobot();
+    void run();
+
+private:
+    // Motors
+    webots::Motor *_left_motor;
+    webots::Motor *_right_motor;
+
+    // Position Sensors
+    webots::PositionSensor *_left_wheel_sensor;
+    webots::PositionSensor *_right_wheel_sensor;
+
+    // Compass
+    webots::Compass *_my_compass;
+
+    // GPS
+    webots::GPS *_my_gps;
+
+    // Distance Sensors
+    webots::DistanceSensor *_distance_sensor[16];
+    const char *ds_name[16] = {"ds0", "ds1", "ds2", "ds3", "ds4", "ds5", "ds6", "ds7", "ds4", "ds9", "ds10", "ds11", "ds12", "ds13", "ds14", "ds15"};
+
+    // Cameras
+    webots::Camera *_front_cam;
+    webots::Camera *_spher_cam;
+
+    // Path Goal
+    float _path_goal[1][2] = {{16, 0}}; // Single goal at (16, 0)
+    int _active_point = 0;
+    const int _total_points = 1;
+
+    // Current position and orientation
+    float _x, _y, _theta;
+
+    // Goal position
+    float _x_goal, _y_goal, _theta_goal;
+
+    // Mode for Bug2 algorithm
+    int _mode; // 0: go-to-goal, 1: wall-following
+
+    // Odometry
+    float _sl, _sr; // Cumulative wheel distances
+
+    // Functions
+    void compute_odometry(bool use_compass = false);
+    double convert_bearing_to_degrees();
+    double convert_bearing_to_radians();
+    double convert_deg_to_rad(double deg);
+    double convert_rad_to_deg(double rad);
+    float encoder_tics_to_meters(float tics);
+    void print_odometry();
+    bool goal_reached();
+    void go_route();
+    float compute_distance_goal();
+    float compute_angle_goal();
+    void head_goal();
+    void move_forward();
+    void stop();
+};
+
+#endif // MY_ROBOT_H
